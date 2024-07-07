0c433b9bb26174708d7d704ab6d95f5a3a6ce49c
sunnamed434 <cetnikletsplay@gmail.com>
2024-07-07 17:20:22 +0300

    Fix CommandAdmin not makes player admin

--- Rocket.Unturned/Commands/CommandAdmin.cs
+++ Rocket.Unturned/Commands/CommandAdmin.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using Rocket.API.Extensions;
 using Rocket.Unturned.Helpers;
 using SDG.Unturned;
+using Steamworks;
 
 namespace Rocket.Unturned.Commands
 {
@@ -49,31 +50,40 @@ namespace Rocket.Unturned.Commands
 
         public void Execute(IRocketPlayer caller, string[] command)
         {
-            if (!R.Settings.Instance.WebPermissions.Enabled)
+            if (R.Settings.Instance.WebPermissions.Enabled)
             {
-                var playerName = command.GetStringParameter(0);
-                if (playerName == null)
-                {
-                    UnturnedChat.Say(caller, U.Translate("command_generic_invalid_parameter"));
-                    throw new WrongUsageOfCommandException(caller, this);
-                }
-                if (RocketUtilities.TryGetSteamIdFromText(playerName, out var steamId) == false)
-                {
-                    UnturnedChat.Say(caller, U.Translate("command_admin_player_invalid", playerName));
-                    throw new WrongUsageOfCommandException(caller, this);
-                }
-                var targetPlayer = UnturnedPlayer.FromCSteamID(steamId!.Value);
-                var targetPlayerName = targetPlayer?.Player != null
-                    ? targetPlayer.CharacterName
-                    : steamId.Value.ToString();
-                if (SteamAdminlist.checkAdmin(steamId!.Value))
-                {
-                    UnturnedChat.Say(caller, U.Translate("command_admin_player_is_admin", targetPlayerName));
-                    return;
-                }
-
-                UnturnedChat.Say(caller, U.Translate("command_admin_success", targetPlayerName));
+                UnturnedChat.Say(caller, $"This command is disabled because {nameof(R.Settings.Instance.WebPermissions)} is enabled.");
+                return;
             }
+
+            var playerName = command.GetStringParameter(0);
+            if (playerName == null)
+            {
+                UnturnedChat.Say(caller, U.Translate("command_generic_invalid_parameter"));
+                throw new WrongUsageOfCommandException(caller, this);
+            }
+            if (RocketUtilities.TryGetSteamIdFromText(playerName, out var steamId) == false)
+            {
+                UnturnedChat.Say(caller, U.Translate("command_admin_player_invalid", playerName));
+                throw new WrongUsageOfCommandException(caller, this);
+            }
+            var targetPlayer = UnturnedPlayer.FromCSteamID(steamId!.Value);
+            var targetPlayerName = targetPlayer?.Player != null
+                ? targetPlayer.CharacterName
+                : steamId.Value.ToString();
+            if (SteamAdminlist.checkAdmin(steamId.Value))
+            {
+                UnturnedChat.Say(caller, U.Translate("command_admin_player_is_admin", targetPlayerName));
+                return;
+            }
+
+            if (RocketUtilities.TryGetSteamIdFromText(caller.Id, out var callerSteamId) == false)
+            {
+                callerSteamId = CSteamID.Nil;
+            }
+
+            SteamAdminlist.admin(steamId.Value, callerSteamId!.Value);
+            UnturnedChat.Say(caller, U.Translate("command_admin_success", targetPlayerName));
         }
     }
 }
